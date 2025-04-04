cd6a0fc75d88da3611303c8b610e756fc4dfa884
Darwin Galindo <dargalindosilva@gmail.com>
2025-04-04 16:11:50 -0500

    testing+

--- src/test/java/com/darwin/abilityservice/infrastructure/output/r2dbc/adapter/AbilityR2dbcAdapterTest.java
+++ src/test/java/com/darwin/abilityservice/infrastructure/output/r2dbc/adapter/AbilityR2dbcAdapterTest.java
@@ -7,6 +7,7 @@ import com.darwin.abilityservice.infrastructure.output.r2dbc.repository.IAbility
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -48,16 +49,14 @@ class AbilityR2dbcAdapterTest {
     }
 
     @Test
-    void paginate() {
+    void paginateAscending() {
         var page = 0;
         var size = 2;
         var sortAscending = true;
         var sortProperty = "name";
-        var sort = Sort.by(Sort.Direction.ASC, sortProperty);
-        var pageable = PageRequest.of(page, size, sort);
 
-        var abilityEntity1 = new AbilityEntity(1L, "Java", "Programming Language", 0);
-        var abilityEntity2 = new AbilityEntity(2L, "Python", "Programming Language", 0);
+        var abilityEntity1 = new AbilityEntity(1L, "Ability 1", "Desc", 0);
+        var abilityEntity2 = new AbilityEntity(2L, "Ability 2", "Desc", 0);
 
         var ability1 = new Ability(abilityEntity1.getId(),
                 abilityEntity1.getName(), abilityEntity1.getDescription(), abilityEntity1.getTechnologiesCount());
@@ -65,7 +64,7 @@ class AbilityR2dbcAdapterTest {
         var ability2 = new Ability(abilityEntity2.getId(),
                 abilityEntity2.getName(), abilityEntity2.getDescription(), abilityEntity2.getTechnologiesCount());
 
-        when(abilityRepository.findBy(pageable)).thenReturn(Flux.just(abilityEntity1, abilityEntity2));
+        when(abilityRepository.findBy(any(Pageable.class))).thenReturn(Flux.just(abilityEntity1, abilityEntity2));
         when(abilityEntityMapper.toModel(abilityEntity1)).thenReturn(ability1);
         when(abilityEntityMapper.toModel(abilityEntity2)).thenReturn(ability2);
 
@@ -74,7 +73,35 @@ class AbilityR2dbcAdapterTest {
                 .expectNext(ability2)
                 .verifyComplete();
 
-        verify(abilityRepository).findBy(pageable);
+        verify(abilityRepository).findBy(any(Pageable.class));
+    }
+
+    @Test
+    void paginateDescending() {
+        var page = 0;
+        var size = 2;
+        var sortAscending = false;
+        var sortProperty = "name";
+
+        var abilityEntity1 = new AbilityEntity(1L, "Ability 1", "Desc", 0);
+        var abilityEntity2 = new AbilityEntity(2L, "Ability 2", "Desc", 0);
+
+        var ability1 = new Ability(abilityEntity1.getId(),
+                abilityEntity1.getName(), abilityEntity1.getDescription(), abilityEntity1.getTechnologiesCount());
+
+        var ability2 = new Ability(abilityEntity2.getId(),
+                abilityEntity2.getName(), abilityEntity2.getDescription(), abilityEntity2.getTechnologiesCount());
+
+        when(abilityRepository.findBy(any(Pageable.class))).thenReturn(Flux.just(abilityEntity2, abilityEntity1));
+        when(abilityEntityMapper.toModel(abilityEntity1)).thenReturn(ability1);
+        when(abilityEntityMapper.toModel(abilityEntity2)).thenReturn(ability2);
+
+        StepVerifier.create(abilityR2DbcAdapter.paginate(page, size, sortProperty, sortAscending))
+                .expectNext(ability2)
+                .expectNext(ability1)
+                .verifyComplete();
+
+        verify(abilityRepository).findBy(any(Pageable.class));
     }
 
     @Test
